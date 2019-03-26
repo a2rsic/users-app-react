aeded1d5e8405c120a352dc0b842540de63fddda
Ana <angy.aa@gmail.com>
2019-03-26 18:43:10 +0100

    Add basic structure

---
+++ src/view/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        App works
+      </div>
+    );
+  }
+}
+
+export default App;
